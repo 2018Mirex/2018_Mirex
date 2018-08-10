52cb482ee51a913b4c2b6adeb8cbed79d795ef4d
rsy1026 <rsy1026@gmail.com>
2018-08-10 15:37:01 +0900

    Comment unused functions

--- mirex_data_SY.py
+++ mirex_data_SY.py
@@ -233,170 +233,170 @@ def save_tfrecords_2(d_inp, d_oup, m_inp, m_oup, fileind, set_):
 	sys.stdout.flush()
 
 
-def save_batches(mode="encdec"): # choose mode between "encdec" and "reg"
-	datapath = '/home/rsy/Documents/MIREX/dataset/PPDD_mono_small'
-	foil_path = os.path.join(datapath, 'cont_foil_npy')
-	true_path = os.path.join(datapath, 'cont_true_npy')
-	prime_path = os.path.join(datapath, 'prime_npy')
-	descrip_path = os.path.join(datapath, 'descriptor_npy')
-	all_path = [prime_path, true_path, foil_path]
-	unit = [0.0, 0.08, 0.17, 0.25, 0.33, 0.42,
-			0.5, 0.58, 0.67, 0.75, 0.83, 0.92]
-	unit2ind = dict()
-	for ui, u in enumerate(unit):
-		unit2ind[u] = ui
-	frame = 12
-	print('---SAVING BATCHES---')
-	for ai, a in enumerate(all_path):
-		filelist = sorted(glob(os.path.join(a, '*_raw.npy')))
-		typename = a.split('/')[-1][:-4]
-		datalen = len(filelist)
-		index = np.arange(datalen)
-		split_rate = [7, 2, 1]
-		train, val, test = [int(datalen/np.sum(split_rate)) * s for s in split_rate]
-		train_ind, val_ind, test_ind = index[:train], index[train:-test], index[-test:]
-		print('---Saving batches for %s folder---' % typename)
-		for fi, f in enumerate(filelist):
-			fileind = f.split('/')[-1][:-8]
-			all_info = np.load(f)
-			last_meas = np.ceil(all_info[-1][0])
-			if last_meas == all_info[-1][0]: # if last position is already int
-				last_meas += 1
-			# make piano roll
-			piano_roll_note = np.zeros([int(last_meas)*frame,])
-			for ii, info in enumerate(all_info):
-				pos, mnn, mpn, dur, chn = info
-				# parse measure position into frame position
-				pos_int = int(pos)
-				pos_unit = np.round(pos - pos_int, 2)
-				if pos_unit not in unit: # quantize into one of 12 units
-					dist = [np.abs(pos_unit - u) for u in unit]
-					pos_ind = dist.index(np.min(dist))
-				else: pos_ind = unit2ind[pos_unit]
-				pos_ = pos_int * frame + pos_ind
-				# decide start position
-				if ai == 0 and ii == 0: # if prime
-					first_pos = pos_int * frame
-				elif ai > 0 and ii == 0: # if cont
-					first_pos = pos_
-				'''
-				if prime, piano roll starts from the beginning of the first measure
-				if cont, piano roll starts from the original position in the first measure
-				(since cont is continuation of the prime)
-				'''
-				# parse duration into number of frames
-				if ii < len(all_info)-1:
-					dur = all_info[ii+1][0] - all_info[ii][0]
-				elif ii == len(all_info)-1:
-					dur = dur
-				dur_int = int(dur)
-				dur_unit = np.round(dur - dur_int, 2)
-				if dur_unit not in unit: # quantize into one of 12 units
-					dist = [np.abs(dur_unit - u) for u in unit]
-					dur_ind = dist.index(np.min(dist))
-				else: dur_ind = unit2ind[dur_unit]
-				dur_ = dur_int * frame + dur_ind
-				# parse midi number into range 0-87
-				mnn_ = int(mnn) - 21
-				piano_roll_note[pos_:pos_+dur_] = mnn_
-			piano_roll = piano_roll_note[first_pos:]
-			# make input and output files and save them
-			if ai == 0: # if prime:
-				# split data into batches with shorter length
-				if mode == "encdec": # for enc-dec structure
-					inp1_batch, inp2_batch, oup_batch = make_batches(piano_roll, mode=mode)
-					# save batches into tfrecord files for training
-					if fi in train_ind:
-						save_tfrecords(inp1_batch, inp2_batch, oup_batch, fileind, 'train', mode=mode)
-					elif fi in val_ind:
-						save_tfrecords(inp1_batch, inp2_batch, oup_batch, fileind, 'val', mode=mode)
-					elif fi in test_ind:
-						savepath = os.path.join(datapath, 'test_%s' % mode)
-						if not os.path.exists(savepath):
-							os.makedirs(savepath)
-						np.save(os.path.join(savepath,'%s_test_inp1.npy' % fileind), inp1_batch)
-						np.save(os.path.join(savepath,'%s_test_inp2.npy' % fileind), inp2_batch)
-						np.save(os.path.join(savepath,'%s_test_oup.npy' % fileind), oup_batch)
-						np.save(os.path.join(savepath,'%s_test_all.npy' % fileind), piano_roll)
-				elif mode == "reg": # for regression
-					inp_batch, _, oup_batch = make_batches(piano_roll, mode=mode)
-					# save batches into tfrecord files for training
-					if fi in train_ind:
-						save_tfrecords(inp_batch, None, oup_batch, fileind, 'train', mode=mode)
-					elif fi in val_ind:
-						save_tfrecords(inp_batch, None, oup_batch, fileind, 'val', mode=mode)
-					elif fi in test_ind:
-						savepath = os.path.join(datapath, 'test_%s' % mode)
-						if not os.path.exists(savepath):
-							os.makedirs(savepath)
-						np.save(os.path.join(savepath,'%s_test_inp.npy' % fileind), inp_batch)
-						np.save(os.path.join(savepath,'%s_test_oup.npy' % fileind), oup_batch)
-						np.save(os.path.join(savepath,'%s_test_all.npy' % fileind), piano_roll)
-				print('saved %ith tfrecord set (mode: %s)' % (fi+1, mode), end='\r')
-			elif ai > 0: # if cont
-				savepath = os.path.join(datapath, 'piano_roll', typename)
-				if not os.path.exists(savepath):
-					os.makedirs(savepath)
-				np.save(os.path.join(savepath, '%s_piano_roll.npy' % fileind), piano_roll)
-				print('saved %ith piano roll files' % (fi+1), end='\r')
-
-def make_batches(data, mode):
-	maxlen = 96 # 8 measures
-	dim = 89
-	datalen = len(data)
-	stride = 6
-	inp1_batch = list()
-	inp2_batch = list()
-	oup_batch = list()
-	if mode == "encdec":
-		for i in range(0, datalen, stride):
-			inp1_batch.append(data[i:i+maxlen])
-			inp2_batch.append(data[i+maxlen-1:i+maxlen*2-1])
-			oup_batch.append(data[i+maxlen:i+maxlen*2])
-	elif mode == "reg":
-		for i in range(0, datalen, stride):
-			inp1_batch.append(data[i:i+maxlen])
-			oup_batch.append(data[i+1:i+maxlen+1])
-	inp1_batch = pad_sequences(inp1_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
-	oup_batch = pad_sequences(oup_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
-	if mode == "encdec":
-		inp2_batch = pad_sequences(inp2_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
-	else: inp2_batch = None
-	return inp1_batch, inp2_batch, oup_batch
-
-def save_tfrecords(inp1, inp2, oup, fileind, set_, mode):
-	# func for saving values into bytes
-	def _bytes_feature(value):
-		return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-	# save tfrecord files
-	datalen = len(inp1)
-	savepath = '/home/rsy/Documents/MIREX/dataset/PPDD_mono_small/tfrecords_%s/%s' % (mode, set_)
-	if not os.path.exists(savepath):
-		os.makedirs(savepath)
-	filename = os.path.join(savepath, '%s.tfrecords' % fileind)
-	writer = tf.python_io.TFRecordWriter(filename)
-	if mode == "encdec":
-		for i in range(datalen):
-			inp1_ = inp1[i]
-			inp2_ = inp2[i]
-			oup_ = oup[i]
-			feature_ = {'inp1': _bytes_feature(tf.compat.as_bytes(inp1_.tostring())),
-						'inp2': _bytes_feature(tf.compat.as_bytes(inp2_.tostring())),
-						'oup': _bytes_feature(tf.compat.as_bytes(oup_.tostring()))}
-			example_ = tf.train.Example(features=tf.train.Features(feature=feature_))
-			writer.write(example_.SerializeToString())
-		writer.close()
-		sys.stdout.flush()
-	elif mode == "reg":
-		for i in range(datalen):
-			inp1_ = inp1[i]
-			oup_ = oup[i]
-			feature_ = {'inp': _bytes_feature(tf.compat.as_bytes(inp1_.tostring())),
-						'oup': _bytes_feature(tf.compat.as_bytes(oup_.tostring()))}
-			example_ = tf.train.Example(features=tf.train.Features(feature=feature_))
-			writer.write(example_.SerializeToString())
-		writer.close()
-		sys.stdout.flush()
+# def save_batches(mode="encdec"): # choose mode between "encdec" and "reg"
+# 	datapath = '/home/rsy/Documents/MIREX/dataset/PPDD_mono_small'
+# 	foil_path = os.path.join(datapath, 'cont_foil_npy')
+# 	true_path = os.path.join(datapath, 'cont_true_npy')
+# 	prime_path = os.path.join(datapath, 'prime_npy')
+# 	descrip_path = os.path.join(datapath, 'descriptor_npy')
+# 	all_path = [prime_path, true_path, foil_path]
+# 	unit = [0.0, 0.08, 0.17, 0.25, 0.33, 0.42,
+# 			0.5, 0.58, 0.67, 0.75, 0.83, 0.92]
+# 	unit2ind = dict()
+# 	for ui, u in enumerate(unit):
+# 		unit2ind[u] = ui
+# 	frame = 12
+# 	print('---SAVING BATCHES---')
+# 	for ai, a in enumerate(all_path):
+# 		filelist = sorted(glob(os.path.join(a, '*_raw.npy')))
+# 		typename = a.split('/')[-1][:-4]
+# 		datalen = len(filelist)
+# 		index = np.arange(datalen)
+# 		split_rate = [7, 2, 1]
+# 		train, val, test = [int(datalen/np.sum(split_rate)) * s for s in split_rate]
+# 		train_ind, val_ind, test_ind = index[:train], index[train:-test], index[-test:]
+# 		print('---Saving batches for %s folder---' % typename)
+# 		for fi, f in enumerate(filelist):
+# 			fileind = f.split('/')[-1][:-8]
+# 			all_info = np.load(f)
+# 			last_meas = np.ceil(all_info[-1][0])
+# 			if last_meas == all_info[-1][0]: # if last position is already int
+# 				last_meas += 1
+# 			# make piano roll
+# 			piano_roll_note = np.zeros([int(last_meas)*frame,])
+# 			for ii, info in enumerate(all_info):
+# 				pos, mnn, mpn, dur, chn = info
+# 				# parse measure position into frame position
+# 				pos_int = int(pos)
+# 				pos_unit = np.round(pos - pos_int, 2)
+# 				if pos_unit not in unit: # quantize into one of 12 units
+# 					dist = [np.abs(pos_unit - u) for u in unit]
+# 					pos_ind = dist.index(np.min(dist))
+# 				else: pos_ind = unit2ind[pos_unit]
+# 				pos_ = pos_int * frame + pos_ind
+# 				# decide start position
+# 				if ai == 0 and ii == 0: # if prime
+# 					first_pos = pos_int * frame
+# 				elif ai > 0 and ii == 0: # if cont
+# 					first_pos = pos_
+# 				'''
+# 				if prime, piano roll starts from the beginning of the first measure
+# 				if cont, piano roll starts from the original position in the first measure
+# 				(since cont is continuation of the prime)
+# 				'''
+# 				# parse duration into number of frames
+# 				if ii < len(all_info)-1:
+# 					dur = all_info[ii+1][0] - all_info[ii][0]
+# 				elif ii == len(all_info)-1:
+# 					dur = dur
+# 				dur_int = int(dur)
+# 				dur_unit = np.round(dur - dur_int, 2)
+# 				if dur_unit not in unit: # quantize into one of 12 units
+# 					dist = [np.abs(dur_unit - u) for u in unit]
+# 					dur_ind = dist.index(np.min(dist))
+# 				else: dur_ind = unit2ind[dur_unit]
+# 				dur_ = dur_int * frame + dur_ind
+# 				# parse midi number into range 0-87
+# 				mnn_ = int(mnn) - 21
+# 				piano_roll_note[pos_:pos_+dur_] = mnn_
+# 			piano_roll = piano_roll_note[first_pos:]
+# 			# make input and output files and save them
+# 			if ai == 0: # if prime:
+# 				# split data into batches with shorter length
+# 				if mode == "encdec": # for enc-dec structure
+# 					inp1_batch, inp2_batch, oup_batch = make_batches(piano_roll, mode=mode)
+# 					# save batches into tfrecord files for training
+# 					if fi in train_ind:
+# 						save_tfrecords(inp1_batch, inp2_batch, oup_batch, fileind, 'train', mode=mode)
+# 					elif fi in val_ind:
+# 						save_tfrecords(inp1_batch, inp2_batch, oup_batch, fileind, 'val', mode=mode)
+# 					elif fi in test_ind:
+# 						savepath = os.path.join(datapath, 'test_%s' % mode)
+# 						if not os.path.exists(savepath):
+# 							os.makedirs(savepath)
+# 						np.save(os.path.join(savepath,'%s_test_inp1.npy' % fileind), inp1_batch)
+# 						np.save(os.path.join(savepath,'%s_test_inp2.npy' % fileind), inp2_batch)
+# 						np.save(os.path.join(savepath,'%s_test_oup.npy' % fileind), oup_batch)
+# 						np.save(os.path.join(savepath,'%s_test_all.npy' % fileind), piano_roll)
+# 				elif mode == "reg": # for regression
+# 					inp_batch, _, oup_batch = make_batches(piano_roll, mode=mode)
+# 					# save batches into tfrecord files for training
+# 					if fi in train_ind:
+# 						save_tfrecords(inp_batch, None, oup_batch, fileind, 'train', mode=mode)
+# 					elif fi in val_ind:
+# 						save_tfrecords(inp_batch, None, oup_batch, fileind, 'val', mode=mode)
+# 					elif fi in test_ind:
+# 						savepath = os.path.join(datapath, 'test_%s' % mode)
+# 						if not os.path.exists(savepath):
+# 							os.makedirs(savepath)
+# 						np.save(os.path.join(savepath,'%s_test_inp.npy' % fileind), inp_batch)
+# 						np.save(os.path.join(savepath,'%s_test_oup.npy' % fileind), oup_batch)
+# 						np.save(os.path.join(savepath,'%s_test_all.npy' % fileind), piano_roll)
+# 				print('saved %ith tfrecord set (mode: %s)' % (fi+1, mode), end='\r')
+# 			elif ai > 0: # if cont
+# 				savepath = os.path.join(datapath, 'piano_roll', typename)
+# 				if not os.path.exists(savepath):
+# 					os.makedirs(savepath)
+# 				np.save(os.path.join(savepath, '%s_piano_roll.npy' % fileind), piano_roll)
+# 				print('saved %ith piano roll files' % (fi+1), end='\r')
+#
+# def make_batches(data, mode):
+# 	maxlen = 96 # 8 measures
+# 	dim = 89
+# 	datalen = len(data)
+# 	stride = 6
+# 	inp1_batch = list()
+# 	inp2_batch = list()
+# 	oup_batch = list()
+# 	if mode == "encdec":
+# 		for i in range(0, datalen, stride):
+# 			inp1_batch.append(data[i:i+maxlen])
+# 			inp2_batch.append(data[i+maxlen-1:i+maxlen*2-1])
+# 			oup_batch.append(data[i+maxlen:i+maxlen*2])
+# 	elif mode == "reg":
+# 		for i in range(0, datalen, stride):
+# 			inp1_batch.append(data[i:i+maxlen])
+# 			oup_batch.append(data[i+1:i+maxlen+1])
+# 	inp1_batch = pad_sequences(inp1_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
+# 	oup_batch = pad_sequences(oup_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
+# 	if mode == "encdec":
+# 		inp2_batch = pad_sequences(inp2_batch, maxlen=maxlen, padding='post', dtype=np.int32, value=88)
+# 	else: inp2_batch = None
+# 	return inp1_batch, inp2_batch, oup_batch
+#
+# def save_tfrecords(inp1, inp2, oup, fileind, set_, mode):
+# 	# func for saving values into bytes
+# 	def _bytes_feature(value):
+# 		return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+# 	# save tfrecord files
+# 	datalen = len(inp1)
+# 	savepath = '/home/rsy/Documents/MIREX/dataset/PPDD_mono_small/tfrecords_%s/%s' % (mode, set_)
+# 	if not os.path.exists(savepath):
+# 		os.makedirs(savepath)
+# 	filename = os.path.join(savepath, '%s.tfrecords' % fileind)
+# 	writer = tf.python_io.TFRecordWriter(filename)
+# 	if mode == "encdec":
+# 		for i in range(datalen):
+# 			inp1_ = inp1[i]
+# 			inp2_ = inp2[i]
+# 			oup_ = oup[i]
+# 			feature_ = {'inp1': _bytes_feature(tf.compat.as_bytes(inp1_.tostring())),
+# 						'inp2': _bytes_feature(tf.compat.as_bytes(inp2_.tostring())),
+# 						'oup': _bytes_feature(tf.compat.as_bytes(oup_.tostring()))}
+# 			example_ = tf.train.Example(features=tf.train.Features(feature=feature_))
+# 			writer.write(example_.SerializeToString())
+# 		writer.close()
+# 		sys.stdout.flush()
+# 	elif mode == "reg":
+# 		for i in range(datalen):
+# 			inp1_ = inp1[i]
+# 			oup_ = oup[i]
+# 			feature_ = {'inp': _bytes_feature(tf.compat.as_bytes(inp1_.tostring())),
+# 						'oup': _bytes_feature(tf.compat.as_bytes(oup_.tostring()))}
+# 			example_ = tf.train.Example(features=tf.train.Features(feature=feature_))
+# 			writer.write(example_.SerializeToString())
+# 		writer.close()
+# 		sys.stdout.flush()
 
 
 # if __name__ == "__main__":
