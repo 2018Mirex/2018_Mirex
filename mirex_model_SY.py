efaf1b2ed57decdc716c3c849a6f7e2880617498
Seungyeon Rhyu <rsy1026@gmail.com>
2018-08-10 20:39:14 +0900

    Fix line breaks online

--- mirex_model_SY.py
+++ mirex_model_SY.py
@@ -8,29 +8,29 @@ import numpy as np
 def look_up(input_dim, emb_dim, scope):
 	with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
 		lookup_table = tf.get_variable('lookup',
-									   dtype=tf.float32,
-									   shape=[input_dim, emb_dim],
-									   initializer=tf.random_normal_initializer())
+						dtype=tf.float32,
+						shape=[input_dim, emb_dim],
+						initializer=tf.random_normal_initializer())
 		return lookup_table
 
 def GRU(inp, num_unit, scope):
 	with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
 		gru_cell = tf.nn.rnn_cell.GRUCell(num_units=num_unit,
-										  kernel_initializer=tf.random_normal_initializer())
+						  kernel_initializer=tf.random_normal_initializer())
 		enc_length = length(inp)
 		gru_out, gru_states = tf.nn.dynamic_rnn(gru_cell, inp,
-												sequence_length=enc_length,
-												dtype=tf.float32)
+							sequence_length=enc_length,
+							dtype=tf.float32)
 		return gru_out, gru_states
 
 def biGRU(inp, num_unit, scope):
 	with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
 		gru_cell = tf.nn.rnn_cell.GRUCell(num_units=num_unit,
-										  kernel_initializer=tf.random_normal_initializer())
+						  kernel_initializer=tf.random_normal_initializer())
 		enc_length = length(inp)
 		gru_out, gru_states = tf.nn.bidirectional_dynamic_rnn(gru_cell, gru_cell, inp,
-													  		  sequence_length=enc_length,
-															  dtype=tf.float32)
+								      sequence_length=enc_length,
+								      dtype=tf.float32)
 		return gru_out, gru_states
 
 def conv1d(inp, filters, kernel_size, dilation_rate, padding, scope):
